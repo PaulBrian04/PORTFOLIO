2768fbe45f36dfdf339ca3910139bef5dd637695
PaulBrian04 <153102981+PaulBrian04@users.noreply.github.com>
2025-04-29 08:33:27 +0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,25 @@
+const hamburger = document.querySelector('.header .nav-bar .nav-list .hamburger');
+const mobile_menu = document.querySelector('.header .nav-bar .nav-list ul');
+const menu_item = document.querySelectorAll('.header .nav-bar .nav-list ul li a');
+const header = document.querySelector('#header .header'); // <-- ito inayos ko
+
+hamburger.addEventListener('click', () => {
+	hamburger.classList.toggle('active');
+	mobile_menu.classList.toggle('active');
+});
+
+document.addEventListener('scroll', () => {
+	var scroll_position = window.scrollY;
+	if (scroll_position > 50) { // pwede mo adjust 50 or 250, depende sayo
+		header.style.backgroundColor = '#313131'; // solid black
+	} else {
+		header.style.backgroundColor = '#313131'; // transparent pag nasa taas
+	}
+});
+
+menu_item.forEach((item) => {
+	item.addEventListener('click', () => {
+		hamburger.classList.toggle('active');
+		mobile_menu.classList.toggle('active');
+	});
+});
